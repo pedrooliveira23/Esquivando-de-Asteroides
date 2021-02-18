bfb5c3957e611e0b13f0e9f9851b7290bd590f60
Pedro de Oliveira <pedrooliveira2309@gmail.com>
2021-02-17 21:53:52 -0300

    Commit Inicial
    Commit Inicial

---
+++ Assets/Scripts/ScriptGameMaster.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class ScriptGameMaster : MonoBehaviour
+{
+
+    public GameObject prefabAsteroide;
+    public static int pontuacao = 0;
+    public static Boolean vivo = true;
+    public static int tempo = 0;
+    public static float tick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("contaPontuacao");
+        StartCoroutine("ondaDeAsteroides");
+        StartCoroutine("ondaDeAsteroidesNoPlayer");
+
+        for(int i = 0; i <= 300; i++)
+        {
+            criarAsteroidesInicio();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!vivo)
+        {
+            StartCoroutine("transicaoScene");
+        }
+
+        tick = 1f / (tempo/4);
+    }
+
+    private void criarAsteroides()
+    {
+        prefabAsteroide.transform.localScale = new Vector3(UnityEngine.Random.Range(1.0f, 20.0f), UnityEngine.Random.Range(1.0f, 20.0f), UnityEngine.Random.Range(1.0f, 20.0f));
+        Instantiate(prefabAsteroide, new Vector3(-3000, UnityEngine.Random.Range(-500, 500), UnityEngine.Random.Range(-500, 500)), Quaternion.identity);
+        prefabAsteroide.transform.localScale = new Vector3(UnityEngine.Random.Range(1.0f, 10.0f), UnityEngine.Random.Range(1.0f, 10.0f), UnityEngine.Random.Range(1.0f, 10.0f));
+        Instantiate(prefabAsteroide, new Vector3(-3000, UnityEngine.Random.Range(-250, 250), UnityEngine.Random.Range(-250, 250)), Quaternion.identity);
+        prefabAsteroide.transform.localScale = new Vector3(UnityEngine.Random.Range(1.0f, 10.0f), UnityEngine.Random.Range(1.0f, 10.0f), UnityEngine.Random.Range(1.0f, 10.0f));
+        Instantiate(prefabAsteroide, new Vector3(-3000, UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(-50, 50)), Quaternion.identity);
+        prefabAsteroide.transform.localScale = new Vector3(UnityEngine.Random.Range(1.0f, 5.0f), UnityEngine.Random.Range(1.0f, 5.0f), UnityEngine.Random.Range(1.0f, 5.0f));
+        Instantiate(prefabAsteroide, new Vector3(3000, UnityEngine.Random.Range(GameObject.Find("Player").transform.position.y - 25, GameObject.Find("Player").transform.position.y + 25), UnityEngine.Random.Range(GameObject.Find("Player").transform.position.z - 25, GameObject.Find("Player").transform.position.z + 25)), Quaternion.identity);
+
+    }
+
+    private void criarAsteroidesNoPlayer()
+    {
+        prefabAsteroide.transform.localScale = new Vector3(UnityEngine.Random.Range(1.0f, 10.0f), UnityEngine.Random.Range(1.0f, 10.0f), UnityEngine.Random.Range(1.0f, 10.0f));
+        Instantiate(prefabAsteroide, new Vector3(-3000, GameObject.Find("Player").transform.position.y, GameObject.Find("Player").transform.position.z - 1), Quaternion.identity);
+    }
+
+    private void criarAsteroidesInicio()
+    {
+        prefabAsteroide.transform.localScale = new Vector3(UnityEngine.Random.Range(1.0f, 20.0f), UnityEngine.Random.Range(1.0f, 20.0f), UnityEngine.Random.Range(1.0f, 20.0f));
+        Instantiate(prefabAsteroide, new Vector3(UnityEngine.Random.Range(-100,-3000), UnityEngine.Random.Range(-500, 500), UnityEngine.Random.Range(-500, 500)), Quaternion.identity);
+        prefabAsteroide.transform.localScale = new Vector3(UnityEngine.Random.Range(1.0f, 10.0f), UnityEngine.Random.Range(1.0f, 10.0f), UnityEngine.Random.Range(1.0f, 10.0f));
+        Instantiate(prefabAsteroide, new Vector3(UnityEngine.Random.Range(-100, -3000), UnityEngine.Random.Range(-250, 250), UnityEngine.Random.Range(-250, 250)), Quaternion.identity);
+        prefabAsteroide.transform.localScale = new Vector3(UnityEngine.Random.Range(1.0f, 5.0f), UnityEngine.Random.Range(1.0f, 5.0f), UnityEngine.Random.Range(1.0f, 5.0f));
+        Instantiate(prefabAsteroide, new Vector3(UnityEngine.Random.Range(-100, -3000), UnityEngine.Random.Range(GameObject.Find("Player").transform.position.y - 25, GameObject.Find("Player").transform.position.y + 25), UnityEngine.Random.Range(GameObject.Find("Player").transform.position.z - 25, GameObject.Find("Player").transform.position.z + 25)), Quaternion.identity);
+    }
+
+    IEnumerator ondaDeAsteroides()
+    {
+        while (true)
+        {
+            if(float.IsInfinity(tick)) { 
+                yield return new WaitForSeconds(1f);
+            } else
+            {
+                yield return new WaitForSeconds(tick);
+            }
+            criarAsteroides();
+        }
+    }
+
+    IEnumerator ondaDeAsteroidesNoPlayer()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(tick,15.0f));
+            criarAsteroidesNoPlayer();
+        }
+    }
+
+    IEnumerator contaPontuacao()
+    {
+        while (true && vivo)
+        {
+            yield return new WaitForSeconds(1f);
+            pontuacao++;
+            tempo++;
+        }
+    }
+
+    IEnumerator transicaoScene()
+    {
+        if (vivo == false)
+        {
+            yield return new WaitForSeconds(3f);
+            SceneManager.LoadScene("SceneGameOver");
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptGameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptGameOver : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 40), "Você perdeu! Pontuação: " + ScriptGameMaster.pontuacao);
+        if (GUI.Button(new Rect(10, 70, 120, 40), "Jogar Novamente"))
+        {
+            ReiniciarJogo();
+        }
+        if (GUI.Button(new Rect(10, 120, 120, 40), "Sair do Jogo"))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void ReiniciarJogo()
+    {
+        ScriptGameMaster.pontuacao = 0;
+        ScriptNave.velocidade = 30f;
+        ScriptGameMaster.vivo = true;
+        ScriptGameMaster.tempo = 0;
+        SceneManager.LoadScene("SceneGamePlay");
+    }
+}
