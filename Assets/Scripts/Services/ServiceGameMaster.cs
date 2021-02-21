06b3f1f898e7419b79f61584a71c93377be3a569
Pedro de Oliveira <pedrooliveira2309@gmail.com>
2021-02-21 00:45:41 -0300

    Ajustando espaçamento entre asteroides

--- Assets/Scripts/Services/ServiceGameMaster.cs
+++ Assets/Scripts/Services/ServiceGameMaster.cs
@@ -115,9 +115,9 @@ public class ServiceGameMaster : MonoBehaviour
             {
                 yield return new WaitForSeconds(gameMaster.getWaveTime());
             }
-            createAsteroid(new Vector3(Random.Range(1.0f, 20.0f), Random.Range(1.0f, 20.0f), Random.Range(1.0f, 20.0f)), new Vector3(-3000, Random.Range(-250, 250), Random.Range(-250, 250)));
-            createAsteroid(new Vector3(Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f)), new Vector3(-3000, Random.Range(-125, 125), Random.Range(-125, 125)));
-            createAsteroid(new Vector3(Random.Range(1.0f, 5.0f), Random.Range(1.0f, 5.0f), Random.Range(1.0f, 5.0f)), new Vector3(-3000, Random.Range(-25, 25), Random.Range(-25, 25)));
+            createAsteroid(new Vector3(Random.Range(1.0f, 20.0f), Random.Range(1.0f, 20.0f), Random.Range(1.0f, 20.0f)), new Vector3(-3000, Random.Range(-500, 500), Random.Range(-500, 500)));
+            createAsteroid(new Vector3(Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f)), new Vector3(-3000, Random.Range(-250, 250), Random.Range(-250, 250)));
+            createAsteroid(new Vector3(Random.Range(1.0f, 5.0f), Random.Range(1.0f, 5.0f), Random.Range(1.0f, 5.0f)), new Vector3(-3000, Random.Range(-50, 50), Random.Range(-50, 50)));
         }
     }
 
@@ -135,7 +135,7 @@ public class ServiceGameMaster : MonoBehaviour
         {
             createAsteroid(new Vector3(Random.Range(1.0f, 20.0f), Random.Range(1.0f, 20.0f), Random.Range(1.0f, 20.0f)), new Vector3(Random.Range(-100, -3000), Random.Range(-500, 500), Random.Range(-500, 500)));
             createAsteroid(new Vector3(Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f)), new Vector3(Random.Range(-100, -3000), Random.Range(-250, 250), Random.Range(-250, 250)));
-            createAsteroid(new Vector3(Random.Range(1.0f, 5.0f), Random.Range(1.0f, 5.0f), Random.Range(1.0f, 5.0f)), new Vector3(Random.Range(-100, -3000), Random.Range(GameObject.Find("Ship").transform.position.y - 25, GameObject.Find("Ship").transform.position.y + 25), Random.Range(GameObject.Find("Ship").transform.position.z - 25, GameObject.Find("Ship").transform.position.z + 25)));
+            createAsteroid(new Vector3(Random.Range(1.0f, 5.0f), Random.Range(1.0f, 5.0f), Random.Range(1.0f, 5.0f)), new Vector3(Random.Range(-100, -3000), Random.Range(GameObject.Find("Ship").transform.position.y - 50, GameObject.Find("Ship").transform.position.y + 50), Random.Range(GameObject.Find("Ship").transform.position.z - 50, GameObject.Find("Ship").transform.position.z + 50)));
         }
     }
 }
